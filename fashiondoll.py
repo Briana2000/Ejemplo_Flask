471cd5deb881e3b4a51beeed6c23451bd2ba82fe
Z <zeta.hoffman@protonmail.com>
2023-08-13 18:54:03 -0600

    refactor

--- fashiondoll.py
+++ fashiondoll.py
@@ -1,6 +1,20 @@
 from typing import Protocol
 
 class Doll(Protocol):
+	"""	Doll Protocol
+		Description:
+			Protocol for Dolls
+			A Protocol is a class that defines a set of methods
+			that a class must implement.
+			In easy words, a Protocol is an Interface.
+		Attributes:
+			Name : str
+			Type : str
+			Price : float
+			Details : str
+		Methods:
+			__str__ : str
+	"""
 	Name : str
 	Type : str
 	Price : float
@@ -9,13 +23,25 @@ class Doll(Protocol):
 		...
 
 class FashionDoll(Doll):
-	def __init__(self, name, type, price, details):
+	"""	FashionDoll Class
+		Description:
+			Class for Fashion Dolls that implements the Doll Protocol
+		Attributes:
+			Name : str
+			Type : str
+			Price : float
+			Details : str
+		Methods:
+			__init__ : None
+			__str__ : str
+	"""
+	def __init__(self, name:str, Type:str, price : float, details:str) -> None:
 		self.Name = name
-		self.Type = type
-		self.Price = (float)(price)
+		self.Type = Type
+		self.Price = (float)(price) # Cast to float
 		self.Details = details
 
-	def __str__(self):
+	def __str__(self) -> str:
 		return f"Name: {self.Name}, Type: {self.Type}, Price: {self.Price}, Details: {self.Details}"
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -12,13 +12,12 @@ app = Flask(__name__)
 barbie = FashionDoll("Barbie", "Fashion Doll", 1000, "Pink Dress")
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    # return barbie.__str__()
+def index() -> str:
 	return render_template('index.html', doll=barbie)
 
 @app.route('/about', methods=['GET'])
-def about() -> 'html':
-	about_content = "Prueba de envío de strings XD."
+def about() -> str:
+	about_content : str = "Prueba de envío de strings XD."
 	return render_template('about.html', message=about_content)
 
 if __name__ == '__main__':
